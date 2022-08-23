2e40793a0f79757b8ab3e5dd5d416af9d76b19d2
Sergio Gabriel Maqueda <gabrielmakedas@gmail.com>
2022-08-23 09:39:54 -0300

    fix: Boton abrir txt

--- lib/functions/txtOpen.dart
+++ lib/functions/txtOpen.dart
@@ -4,7 +4,7 @@ import 'package:open_file/open_file.dart';
 
 Future<void> openTxt() async {
   final directory = (await getExternalStorageDirectory())?.path;
-  final File file = File('$directory/Log.txt');
-  final fileConvert = file.toString();
+  final file = File('$directory/Log.txt');
+  final fileConvert = file.path;
   OpenFile.open(fileConvert);
 }
